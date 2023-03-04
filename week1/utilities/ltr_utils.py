279f7010ff473e371eba0dd8bb42e8aa93454be4
Alex Kataev <alexander.kataev@wolt.com>
2023-03-04 15:52:12 +0000

    level1

--- week1/utilities/ltr_utils.py
+++ week1/utilities/ltr_utils.py
@@ -10,7 +10,22 @@ def create_rescore_ltr_query(user_query: str, query_obj, click_prior_query: str,
     # Create the base query, use a much bigger window
     #add on the rescore
     ##### Step 4.e:
-    print("IMPLEMENT ME: create_rescore_ltr_query")
+    # print("IMPLEMENT ME: create_rescore_ltr_query")
+    query_obj["rescore"] = {
+        "window_size": rescore_size,
+        "query": {
+            "rescore_query": {
+                "sltr": {
+                    "params": {
+                        "keywords": user_query
+                    },
+                    "model": ltr_model_name,
+                    "store": ltr_store_name
+                }
+            },
+            "rescore_query_weight": "2"
+        }
+    }
     if active_features is not None and len(active_features) > 0:
         query_obj["rescore"]["query"]["rescore_query"]["sltr"]["active_features"] =  active_features
 
@@ -54,11 +69,44 @@ def create_sltr_hand_tuned_query(user_query, query_obj, click_prior_query, ltr_m
         sltr["active_features"] =  active_features
     query_obj["query"]["function_score"]["query"]["bool"]["should"].append(sltr)
     return query_obj, len(query_obj["query"]["function_score"]["query"]["bool"]["should"])
+import pdb
 
 def create_feature_log_query(query, doc_ids, click_prior_query, featureset_name, ltr_store_name, size=200, terms_field="_id"):
     ##### Step 3.b:
-    print("IMPLEMENT ME: create_feature_log_query")
-    return None
+
+    query_obj = {
+            'query': {
+                'bool': {
+                    "filter": [  # use a filter so that we don't actually score anything
+                        {
+                            "terms": {
+                                terms_field: doc_ids
+                            }
+                        },
+                        {  # use the LTR query bring in the LTR feature set
+                            "sltr": {
+                                "_name": "logged_featureset",
+                                "featureset": featureset_name,
+                                "store": ltr_store_name,
+                                "params": {
+                                    "keywords":query
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            # Turn on feature logging so that we get weights back for our features
+            "ext": {
+                "ltr_log": {
+                    "log_specs": {
+                        "name": "log_entry",
+                        "named_query": "logged_featureset"
+                    }
+                }
+            }
+        }
+    return query_obj
 
 
 # Item is a Pandas namedtuple
